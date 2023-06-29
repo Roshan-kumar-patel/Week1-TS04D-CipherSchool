a84cfe00b941bcd2b3e65dda616bb8ca10fea734
Roshan_kumar <roshankumar1patel@gmail.com>
2023-06-29 11:42:18 +0530

    week1

---
+++ Lecture3-CipherSchool/function.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+double power(double base, int exponent) {
+    double result = 1.0;
+    // If exponent is positive
+    if (exponent >= 0) {
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+    }
+    // If exponent is negative
+    else {
+        exponent = -exponent;
+        for (int i = 0; i < exponent; i++) {
+            result /= base;
+        }
+    }
+    return result;
+}
+
+int main() {
+    double base;
+    int exponent;
+
+    cout << "Enter the base: ";
+    cin >> base;
+    cout << "Enter the exponent: ";
+    cin >> exponent;
+
+    double result = power(base, exponent);
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
